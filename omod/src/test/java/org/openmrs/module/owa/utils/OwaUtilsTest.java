52c77926bae0757f7ded3a8b2aa5bab8693cd0f2
Ethan Butt <ethanbutt309@gmail.com>
2019-04-05 08:50:14 -0500

    Added two unit tests for new OWA util method; modified existing tests with more variability and naming consistancy.

--- omod/src/test/java/org/openmrs/module/owa/utils/OwaUtilsTest.java
+++ omod/src/test/java/org/openmrs/module/owa/utils/OwaUtilsTest.java
@@ -4,26 +4,47 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class OwaUtilsTest {
-	
+
 	@Test
-	public void testgetFileNameWithNoQueryString() {
+	public void testGetFileNameWithNoQueryString() {
 		String testString = "https://dl.bintray.com/openmrs/owa/cohortbuilder-1.0.0-beta.zip";
 		String fileName = OwaUtils.getFileName(testString);
 		Assert.assertEquals(fileName, "cohortbuilder.zip");
 	}
-	
+
 	@Test
-	public void testgetFileNameWithQueryString() {
+	public void testGetFileNameWithQueryString() {
 		String testString = "https://bintray.com/openmrs/owa/download_file?file_path=cohortbuilder-1.0.0-beta.zip";
 		String fileName = OwaUtils.getFileName(testString);
 		Assert.assertEquals(fileName, "cohortbuilder.zip");
 	}
-	
+
 	@Test
-	public void testRemoveVersionNumber() {
+	public void testRemoveVersionNumberStandard() {
+		String testString = "cohortbuilder-1.0.0.zip";
+		String fileName = OwaUtils.removeVersionNumber(testString);
+		Assert.assertEquals(fileName, "cohortbuilder.zip");
+	}
+
+	@Test
+	public void testRemoveVersionNumberBeta() {
 		String testString = "cohortbuilder-1.0.0-beta.zip";
 		String fileName = OwaUtils.removeVersionNumber(testString);
 		Assert.assertEquals(fileName, "cohortbuilder.zip");
 	}
-	
+
+	@Test
+	public void testGetStrippedFileNameStandard() {
+		String testString = "cohortbuilder-1.0.0.zip";
+		String fileName = OwaUtils.getStrippedFileName(testString);
+		Assert.assertEquals(fileName, "cohortbuilder");
+	}
+
+	@Test
+	public void testGetStrippedFileNameBeta() {
+		String testString = "cohortbuilder-1.0.0-beta.zip";
+		String fileName = OwaUtils.getStrippedFileName(testString);
+		Assert.assertEquals(fileName, "cohortbuilder");
+	}
+
 }
